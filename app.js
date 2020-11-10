17b1a9df362154800a8c1205e2e78ed5d3925b1f
spncr <spencerwberry@gmail.com>
2020-11-09 16:59:04 -0800

    Removed Port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const indexRouter = require('./routes/index')
 const newRouter = require('./routes/new')
 
 const app = express()
-const port = 3000
+//const port = 3000
 
 //views
 app.set('views', path.join(__dirname, 'views'))
@@ -13,12 +13,12 @@ app.set('view engine', 'pug')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, 'public')))
+app.set(express.static(path.join(__dirname, 'public')))
 
 //routes
 app.use('/', indexRouter)
 app.use('/new', newRouter)
 
-app.listen(port, () => console.log(`We listening at ${port}!`))
+//app.listen(port, () => console.log(`We listening at ${port}!`))
 
 module.exports = app
